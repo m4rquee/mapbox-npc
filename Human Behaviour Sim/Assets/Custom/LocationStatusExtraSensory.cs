c8f8d02af0a71a49970e1690d9d184958021bace
m4rquee <lucasxm12@gmail.com>
2022-03-28 10:47:19 -0300

    selected npc highlight and dismiss button working

--- Human Behaviour Sim/Assets/Custom/LocationStatusExtraSensory.cs
+++ Human Behaviour Sim/Assets/Custom/LocationStatusExtraSensory.cs
@@ -1,5 +1,4 @@
-﻿using Mapbox.Unity.Location;
-using Mapbox.Utils;
+﻿using Mapbox.Utils;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,20 +8,51 @@ namespace Custom
     {
         [SerializeField] private Text statusText;
 
-        private AbstractLocationProvider _locationProvider;
+        [SerializeField] private Material baseMaterial;
 
-        private void Start()
+        [SerializeField] private Material selectedMaterial;
+
+        [SerializeField] private GameObject canvas;
+
+        private Camera _camera;
+        private LocationProviderExtraSensory _selectedNpc;
+
+        private void Awake()
         {
-            if (_locationProvider != null) return;
-            _locationProvider =
-                LocationProviderFactory.Instance.DefaultLocationProvider as AbstractLocationProvider;
+            _camera = Camera.main;
         }
-
 
         private void Update()
         {
-            var currLoc = _locationProvider.CurrentLocation;
-            var userState = ((LocationProviderExtraSensory) _locationProvider).UserState;
+            if (_selectedNpc != null) UpdateText();
+            if (!Input.GetMouseButtonDown(0)) return;
+
+            // Unselect the current NPC:
+            if (_selectedNpc != null)
+            {
+                foreach (var r in _selectedNpc.GetComponentsInChildren<Renderer>())
+                    r.material = baseMaterial;
+                _selectedNpc = null;
+                statusText.text = "";
+            }
+
+            // Check if clicked on a NPC:
+            var hit = Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out var hitInfo);
+            if (!hit || hitInfo.transform == null || !hitInfo.transform.gameObject.CompareTag("NPC")) return;
+
+            // Select the clicked NPC:
+            _selectedNpc = hitInfo.transform.gameObject.GetComponentInParent<LocationProviderExtraSensory>();
+            foreach (var r in _selectedNpc.GetComponentsInChildren<Renderer>())
+                r.material = selectedMaterial;
+
+            // Activate the info panel:
+            canvas.SetActive(true);
+        }
+
+        private void UpdateText()
+        {
+            var currLoc = _selectedNpc.CurrentLocation;
+            var userState = _selectedNpc.UserState;
 
             if (currLoc.IsLocationServiceInitializing)
                 statusText.text = "location services are initializing";
@@ -36,7 +66,7 @@ namespace Custom
                         statusText.text = "Waiting for location ....";
                     else
                     {
-                        statusText.text = $"{currLoc.LatitudeLongitude} - LyingDown = {userState.LyingDown}";
+                        statusText.text = $"{currLoc.LatitudeLongitude} \n- LyingDown = {userState.LyingDown}";
                         statusText.color = userState.LyingDown ? Color.green : Color.red;
                     }
                 }
